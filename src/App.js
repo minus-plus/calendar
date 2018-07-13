f905274165dd8b8611839b9cfe84ecf5591b2773
yun <wangyun732@gmail.com>
2018-07-13 18:36:52 -0400

    material-ui config

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = theme => ({
+  view: {
+    height: '100%',
+    padding: 0
+  }
+})
+
 
 class App extends Component {
-  render() {
+  render () {
+    const { classes } = this.props
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={classes.view}>
+        Material Calendar
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default withStyles(styles)(App)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import 'typeface-roboto'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import orange from '@material-ui/core/colors/deepOrange'
+import lightBlue from '@material-ui/core/colors/lightBlue'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: orange[500],
+      main: orange[800],
+      dark: orange[900]
+    },
+    secondary: {
+      light: lightBlue[400],
+      main: lightBlue[600],
+      dark: lightBlue[800]
+    }
+  },
+  appBar: {
+    height: 50
+  }
+})
+
+const UI = () => (
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>
+)
+
+ReactDOM.render(<UI />, document.getElementById('root'))
+registerServiceWorker()
